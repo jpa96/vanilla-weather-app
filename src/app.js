1694bdc09c4b8c1429c728f0d02f3ae1c98964e0
jpa96 <100251959+jpa96@users.noreply.github.com>
2022-05-03 22:15:14 +0800

    Slidein Animation & KeyFramesComplete
    Resource - https://alvarotrigo.com/blog/css-animations-scroll/

--- src/app.js
+++ src/app.js
@@ -211,7 +211,23 @@ form.addEventListener("submit", handleSubmit);
 
 search("Seattle");
 
-
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 80;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
 
 /*
 function convertUnitF() {
